2e37588a873c48dfdfc6a5e3c3cdb0bf944837f9
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-04-23 14:54:54 -0400

    Two-way Binding

--- meu-primeiro-app/src/app/app.module.ts
+++ meu-primeiro-app/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
@@ -14,7 +16,8 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
