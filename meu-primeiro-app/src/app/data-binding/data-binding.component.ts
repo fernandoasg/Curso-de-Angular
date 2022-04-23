225372f6e466dc09aa2db3120995b631d75abf85
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-04-23 14:39:13 -0400

    Property Binding

--- meu-primeiro-app/src/app/data-binding/data-binding.component.ts
+++ meu-primeiro-app/src/app/data-binding/data-binding.component.ts
@@ -9,6 +9,9 @@ export class DataBindingComponent implements OnInit {
 
   public nome: string = "Fernando";
   public idade: number = 29;
+  public checkedDisabled: boolean = false;
+  public imgSrc: string = "https://sm.ign.com/ign_br/screenshot/default/naruto-shippuden_zy11.jpg";
+  public imgTitle: string = "Naruto";
 
   constructor() { }
 
