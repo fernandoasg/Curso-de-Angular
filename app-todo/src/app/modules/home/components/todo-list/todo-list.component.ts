0b3bea24b6f444c065856bc6d670c474b2ce820e
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-05-11 20:10:09 -0400

    Interface Task e lista de task

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+// Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+
+  public taskList: Array<TaskList> = [
+    { task: "Minha nova Task", checked: true },
+    { task: "Minha nova Task 2", checked: false }
+  ];
 
   constructor() { }
 
