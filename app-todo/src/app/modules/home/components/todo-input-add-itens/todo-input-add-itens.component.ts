4ea4ead62f74e4d37c02cc43784ec2d25c019b2e
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-05-19 18:17:31 -0400

    Adicionando tarefas

--- app-todo/src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ app-todo/src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoInputAddItensComponent implements OnInit {
 
+  @Output() public emitItemTaskList = new EventEmitter();
+
+  public addItemTaskList: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public submitItemTaskList() {
+    this.addItemTaskList = this.addItemTaskList.trim();
+    if (this.addItemTaskList) {
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = "";
+    }
   }
 
 }
--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -10,14 +10,15 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements OnInit {
 
-  public taskList: Array<TaskList> = [
-    { task: "Minha nova Task", checked: true },
-    { task: "Minha nova Task 2", checked: false }
-  ];
+  public taskList: Array<TaskList> = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public setEmitTaskList(event: string) {
+    this.taskList.push({task: event, checked: false});
   }
 
   public deleteItemTaskList(event: number) {
