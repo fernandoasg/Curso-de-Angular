9068cad95e7f2fc599011c2be2e7125fc3b2cf7b
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-07-02 12:57:29 -0400

    Componentes de busca e lista

--- pokedex/src/app/shared/shared.module.ts
+++ pokedex/src/app/shared/shared.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 // Components
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 @NgModule({
   declarations: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent
   ],
   exports: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent
   ],
   imports: [
     CommonModule
