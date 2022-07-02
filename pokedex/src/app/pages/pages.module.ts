fde3d6d4a64689e3b2494ed936ff5fbf27b82035
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-07-02 12:45:42 -0400

    Componente header

--- pokedex/src/app/pages/pages.module.ts
+++ pokedex/src/app/pages/pages.module.ts
@@ -7,6 +7,7 @@ import { RoutingModule } from './routing.module';
 // Pages
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
