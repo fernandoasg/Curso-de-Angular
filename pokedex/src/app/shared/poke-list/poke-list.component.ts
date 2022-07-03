3cf4a51f8076535792e7604960a380d80dc86c7a
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-07-03 13:23:23 -0400

    Filtro funcionando

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -8,6 +8,7 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 })
 export class PokeListComponent implements OnInit {
 
+  private setAllPokemons: any;
   public getAllPokemons: any;
 
   constructor(
@@ -17,9 +18,18 @@ export class PokeListComponent implements OnInit {
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
       res => {
-        this.getAllPokemons = res.results;
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
       }
     );
+  }
+
+  public getSearch(value: string) {
+    const filter = this.setAllPokemons.filter( (res: any) => {
+      return res.name.includes(value.toLocaleLowerCase());
+    });
+
+    this.getAllPokemons = filter;
   }
 
 }
