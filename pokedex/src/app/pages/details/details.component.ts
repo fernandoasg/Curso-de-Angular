de5d91f8986e6be588fe82ac94309b519ff536ed
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-07-03 13:37:06 -0400

    Buscando dados na tela de detalhes

--- pokedex/src/app/pages/details/details.component.ts
+++ pokedex/src/app/pages/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) { }
 
   ngOnInit(): void {
+    this.pokemon;
   }
 
+  get pokemon() {
+
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemon(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
+    
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        console.log(res);
+      }
+    )
+  }
 }
--- pokedex/src/app/service/poke-api.service.ts
+++ pokedex/src/app/service/poke-api.service.ts
@@ -19,7 +19,7 @@ export class PokeApiService {
       tap (res => res),
       tap(res => {
         res.results.map( (resPokemons: any) => {
-          this.apiGetPokemons(resPokemons.url).subscribe(
+          this.apiGetPokemon(resPokemons.url).subscribe(
             res => resPokemons.status = res
           )
         })
@@ -27,7 +27,7 @@ export class PokeApiService {
     );
   }
 
-  public apiGetPokemons(url: string): Observable<any> {
+  public apiGetPokemon(url: string): Observable<any> {
     return this.http.get<any>(url).pipe(
       map(
         res => res
