04f2cc96c7b46c45d7ebeb65fc8f61345e9d7501
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-07-03 14:03:06 -0400

    Mostrando dados de detalhes

--- pokedex/src/app/pages/details/details.component.ts
+++ pokedex/src/app/pages/details/details.component.ts
@@ -14,16 +14,18 @@ export class DetailsComponent implements OnInit {
   private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
   private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon: any;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private pokeApiService: PokeApiService
   ) { }
 
   ngOnInit(): void {
-    this.pokemon;
+    this.getPokemon;
   }
 
-  get pokemon() {
+  get getPokemon() {
 
     const id = this.activatedRoute.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGetPokemon(`${this.urlPokemon}/${id}`);
@@ -31,7 +33,7 @@ export class DetailsComponent implements OnInit {
     
     return forkJoin([pokemon, name]).subscribe(
       res => {
-        console.log(res);
+        this.pokemon = res;
       }
     )
   }
