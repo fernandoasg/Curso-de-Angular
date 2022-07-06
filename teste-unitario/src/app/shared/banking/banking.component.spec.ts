ba8799c01027e8a22d8e86f518c588707ae94469
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-07-05 21:36:22 -0400

    Testes unitários

--- teste-unitario/src/app/shared/banking/banking.component.spec.ts
+++ teste-unitario/src/app/shared/banking/banking.component.spec.ts
@@ -22,4 +22,28 @@ describe('BankingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`(U) getPoupanca(): poupanca should have 10`, () => {
+    expect(component.getPoupanca).toEqual(10);
+  });
+
+  it(`(U) getCarteira(): carteira should have 10`, () => {
+    expect(component.getCarteira).toEqual(50);
+  });
+
+  it(`(U) setSacar(): should transfer poupanca from carteira`, () => {
+    component.setSacar('10');
+    fixture.detectChanges();
+
+    expect(component.getPoupanca).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  });
+
+  it(`(U) setDepositar(): should transfer carteira from poupanca`, () => {
+    component.setDepositar('50');
+    fixture.detectChanges();
+
+    expect(component.getCarteira).toEqual(0);
+    expect(component.getPoupanca).toEqual(60);
+  });
 });
