1e356241074cd705a0cb58f80f13f0a2b7230538
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-07-06 19:42:12 -0400

    Componente lista de investimentos

--- teste-unitario/src/app/shared/banking/banking.component.spec.ts
+++ teste-unitario/src/app/shared/banking/banking.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ListComponent } from '../investiments/list/list.component';
 import { BankingComponent } from './banking.component';
 
 describe('BankingComponent', () => {
@@ -8,7 +9,7 @@ describe('BankingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BankingComponent ]
+      declarations: [ BankingComponent, ListComponent ]
     })
     .compileComponents();
   });
