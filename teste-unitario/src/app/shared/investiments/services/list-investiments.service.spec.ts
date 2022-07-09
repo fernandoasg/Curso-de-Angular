289cf7310931a35c149998b0d42d09f99235c5d7
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-07-08 20:13:43 -0400

    Testando o service

--- teste-unitario/src/app/shared/investiments/services/list-investiments.service.spec.ts
+++ teste-unitario/src/app/shared/investiments/services/list-investiments.service.spec.ts
@@ -54,4 +54,22 @@ describe('ListInvestimentsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('(U) should be listed all investiments', (done) =>{
+    service.list().subscribe(
+      (res: Array<Investiments>) => {
+        expect(res[0].name).toEqual('Banco 1');
+        expect(res[0].value).toEqual(100);
+
+        expect(res[4].name).toEqual('Banco 5');
+        expect(res[4].value).toEqual(100);
+        done();
+      }
+    );
+
+    const req = httpTestingController.expectOne(URL);
+    req.flush(mockList);
+
+    expect(req.request.method).toEqual('GET');
+  });
 });
