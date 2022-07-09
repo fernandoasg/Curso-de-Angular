5f0340e80f3859f56dee532c2ea098f68c065516
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-07-09 16:00:47 -0400

    Estilo da página de login

--- auth-front/src/app/app.component.ts
+++ auth-front/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <a [routerLink]="['admin']">admin</a>
     <router-outlet></router-outlet>
   `
 })
