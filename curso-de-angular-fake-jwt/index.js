2a58d09f613c54897adad310c90490d906188e0e
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-07-09 14:53:39 -0400

    fake server jwt

---
+++ curso-de-angular-fake-jwt/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/sign", (req, res) => {
+  const email = "dener@vidafullstack.com.br";
+  const password = "123";
+
+  if (req.body.email === email && req.body.password === password) {
+    const data = {
+      nome: "Dener Troquatte",
+      email,
+      role: ["sysAdmin"],
+    };
+
+    const token = jwt.sign({ data }, "SECRET", {
+      expiresIn: 100000,
+    });
+
+    return res.json({ token: token });
+  }
+
+  res.status(500).json({ message: "Usuário ou senha incorreta" });
+});
+
+app.listen(port, () => {
+  console.log(`Link => http://localhost:${port}`);
+});
