4b8204140a2a07d9178ba48323aeb41fd7477854
Fernando Augusto <f3rnand0_lp@hotmail.com>
2022-07-10 12:45:01 -0400

    Tema das páginas mudando

--- dark-theme/src/app/app.module.ts
+++ dark-theme/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
+import { HomeComponent } from './core/pages/components/home/home.component';
+import { SobreComponent } from './core/pages/components/sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
